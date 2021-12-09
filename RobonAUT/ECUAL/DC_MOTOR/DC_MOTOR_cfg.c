25450ac749337036a2dd71fbe62875bd3470acf8
Bagoly Zoltán <zoltan.bagoly@gmail.com>
2021-12-09 21:07:12 +0100

    2 PWM motvez elkezdve:

---
+++ RobonAUT/ECUAL/DC_MOTOR/DC_MOTOR_cfg.c
@@ -0,0 +1,41 @@
+/*
+ * File: DC_MOTOR_cfg.c
+ * Driver Name: [[ DC MOTOR ]]
+ * SW Layer:   ECUAL
+ * Created on: Jun 28, 2020
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ *
+ */
+
+#include "../DC_MOTOR/DC_MOTOR.h"
+
+const DC_MOTOR_CfgType DC_MOTOR_CfgParam[DC_MOTOR_UNITS] =
+{
+	// PWM 1 Configurations
+    {
+	    GPIOB,
+		GPIOB,
+		GPIO_PIN_12,
+		GPIO_PIN_13,
+		TIM8,
+		TIM_CHANNEL_2,	//PIN_57
+		72,
+		DC_MOTOR_F_PWM,
+		DC_MOTOR_PWM_RES
+	},
+    // PWM 1 Configurations
+	{
+		GPIOB,
+		GPIOB,
+		GPIO_PIN_12,
+		GPIO_PIN_13,
+		TIM8,
+		TIM_CHANNEL_3,	//PIN_40
+		72,
+		DC_MOTOR_F_PWM,
+		DC_MOTOR_PWM_RES
+	}
+};
